a29f5cb177b4b5ef79266a4cc6ff91fe2ba32337
Dave Nguyen <daventech102@gmail.com>
2018-08-22 22:06:59 -0500

    commit all

---
+++ src/app/app.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styles: [
+    `
+      .list-group-item:first-child {
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+        border-top: 0;
+      }
+    `
+  ]
+})
+export class AppComponent implements OnInit {
+  query: string;
+  artists: any;
+  currentArtist: object;
+
+  showArtist(item) {
+    this.query = item.name;
+    item.highlight = !item.highlight;
+    this.currentArtist = item;
+  }
+
+  addArtist(value) {
+    if (value!=='') {
+      this.artists.push({
+        name: value,
+        reknown: 'Hard Knocks'
+      });
+    }
+  }
+
+  constructor( private http: HttpClient ) {
+    this.query = '';
+  }
+
+  ngOnInit(): void {
+    this.http.get<Object>('../assets/data.json').subscribe( data => {
+      this.artists = data;
+    });
+  }
+}
